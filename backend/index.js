8844b98fbb419806cc1558099bd9d8917b1242d2
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-10-08 14:36:36 +0500

    Initialize backend with Express and update frontend config
    Set up a basic Express backend with health check endpoint and add essential middlewares (cookie-parser, helmet, cors). In the frontend, switch environment variable to VITE_API_URL and add a health check call to the backend on startup.

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,25 @@
-console.log( process.env.PORT )
+import express from "express"
+import cookieParser from "cookie-parser"
+import helmet from "helmet"
+import cors from "cors"
+
+const PORT = process.env.PORT || 3_000
+
+const app = express()
+
+// Setup middlewares
+app.use( express.json() )
+app.use( cookieParser() )
+app.use( helmet() )
+app.use( cors( {
+	credentials: true,
+} ) )
+
+// Setup routes
+app.get( "/health", ( req, res ) => res.send( {
+	status: "ok",
+	timestamp: new Date().toISOString(),
+} ) )
+
+// Run
+app.listen( PORT, "0.0.0.0", () => console.info( `Server ready at: ${ PORT }` ) )
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,7 @@
 import "@app/css/main.css"
 
-document.title = import.meta.env.VITE_APP_NAME
+const API_URL = import.meta.env.VITE_API_URL
+
+// Health check
+const check = await ( await fetch( `${ API_URL }/health` ) ).json()
+console.table( check )
