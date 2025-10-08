9346eb68127a8cc867e80a3cc0f1107171873a97
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-10-08 17:02:30 +0500

    Add Google Sign-In button

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,42 @@
 import "@app/css/main.css"
 
-const API_URL = import.meta.env.VITE_API_URL
+window.onload = () => {
 
-// Health check
-const check = await ( await fetch( `${ API_URL }/health` ) ).json()
-console.table( check )
+	const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
+
+	google.accounts.id.initialize( {
+		client_id: GOOGLE_CLIENT_ID,
+		callback: handleCredentialResponse,
+		auto_select: false,
+	} )
+
+	google.accounts.id.renderButton(
+		document.getElementById( "g_id_signin" ),
+		{
+			theme: "outline",
+			size: "large",
+		}
+	)
+}
+
+function handleCredentialResponse( response ) {
+
+	const jwt = response.credential
+	const payload = parseJWT( jwt )
+
+	console.log( payload )
+}
+
+function parseJWT( token ) {
+
+	const base64Url = token.split( "." )[ 1 ]
+	const base64 = base64Url.replace( /-/g, "+" ).replace( /_/g, "/" )
+	const jsonPayload = decodeURIComponent(
+		atob( base64 )
+			.split( "" )
+			.map( c => "%" + ( "00" + c.charCodeAt( 0 ).toString( 16 ) ).slice( - 2 ) )
+			.join( "" )
+	)
+
+	return JSON.parse( jsonPayload )
+}
