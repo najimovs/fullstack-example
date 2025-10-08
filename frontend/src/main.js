0c524541108e2e78d367fca6584d9d3e59f6c63d
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-10-08 14:19:37 +0500

    Initialize project with backend and frontend setup
    Add initial backend and frontend project structure. Includes backend entry point and package.json, frontend Vite setup, ESLint config, basic HTML, and supporting files.

---
+++ frontend/src/main.js
@@ -0,0 +1,3 @@
+import "@app/css/main.css"
+
+document.title = import.meta.env.VITE_APP_NAME
---
+++ frontend/vite.config.js
@@ -0,0 +1,23 @@
+import path from "node:path"
+import { defineConfig } from "vite"
+import eslintPlugin from "vite-plugin-eslint"
+
+export default defineConfig( {
+	server: {
+		host: true,
+		port: 3001,
+	},
+	build: {
+		target: "esnext",
+		chunkSizeWarningLimit: 1024,
+	},
+	resolve: {
+		alias: {
+			"@app": path.resolve( __dirname, "./src/app" ),
+			"@lib": path.resolve( __dirname, "./src/library" ),
+		},
+	},
+	plugins: [
+		eslintPlugin(),
+	]
+} )
