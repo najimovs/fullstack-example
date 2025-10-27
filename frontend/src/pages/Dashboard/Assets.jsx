541fd694e63cf3b1589ecf0a2c05d15cf882a8f4
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-10-27 16:25:02 +0500

    Render assets list

--- frontend/src/pages/Dashboard/Assets.jsx
+++ frontend/src/pages/Dashboard/Assets.jsx
@@ -1,15 +1,25 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { NavLink } from "react-router"
 
 export default function () {
+
+	const [ assets, setAssets ] = useState( [] )
 
 	useEffect( () => {
 
 		fetch( "http://localhost:3000/dashboard/assets", { credentials: "include" } )
 		.then( response => response.json() )
-		.then( json => console.log( json ) )
+		.then( json => setAssets( json ) )
 		.catch( error => console.log( error ) )
 
 	}, [] )
 
-	return <>Assets</>
+	return <>
+		<h1>Assets</h1>
+		<ul>
+			{ assets.map( asset => <li key={ asset.id }>
+				<NavLink to={ "/" + asset.resource_path }>{ asset.name }</NavLink>
+			</li> ) }
+		</ul>
+	</>
 }
